95fe72838cc982d702782c14e58af1a9e8ad5503
Ovenkato <89723323+kixx007@users.noreply.github.com>
2023-12-06 07:21:39 +0700

    Update Pipelines.cs

--- Shadowsocks.CLI/Client/Pipelines.cs
+++ Shadowsocks.CLI/Client/Pipelines.cs
@@ -2,7 +2,7 @@ using Shadowsocks.Protocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
+using System.Net; 
 using System.Text;
 using System.Threading.Tasks;
 
