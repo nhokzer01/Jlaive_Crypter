de8bc60acee5ca327ab3fb950d38ab0254cfa844
Uni <89723323+UniRevendBypass@users.noreply.github.com>
2023-12-13 07:50:57 +0700

    Update download_test_data.cmake

--- cmake/download_test_data.cmake
+++ cmake/download_test_data.cmake
@@ -2,7 +2,7 @@ set(JSON_TEST_DATA_URL     https://github.com/nlohmann/json_test_data)
 set(JSON_TEST_DATA_VERSION 3)
 
 # if variable is set, use test data from given directory rather than downloading them
-if(JSON_TestDataDirectory) 
+if(JSON_TestDataDirectory)  
     message(STATUS "Using test data in ${JSON_TestDataDirectory}.")
     add_custom_target(download_test_data)
     file(WRITE ${CMAKE_BINARY_DIR}/include/test_data.hpp "#define TEST_DATA_DIRECTORY \"${JSON_TestDataDirectory}\"\n")
