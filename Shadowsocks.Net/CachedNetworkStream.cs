9fd58df862299390bd4813013bd03cd0a5746ea2
Uni <89723323+UniRevendBypass@users.noreply.github.com>
2024-01-13 09:55:11 +0700

    Update CachedNetworkStream.cs

--- Shadowsocks.Net/CachedNetworkStream.cs
+++ Shadowsocks.Net/CachedNetworkStream.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;   
-
+ 
 namespace Shadowsocks
 {
     // cache first packet for duty-chain pattern listener
